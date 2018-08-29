51363fab108e3771aedd334605f67ff3da3c29fc
ricardoantonello <ricardo@antonello.com.br>
2018-08-29 11:19:51 +0000

    mvp

--- estaciona.py
+++ estaciona.py
@@ -6,89 +6,42 @@
 # import the necessary packages
 import time
 import cv2
-print(cv2.__version__)
-import numpy as np
-from matplotlib import pyplot as plt
 
-#import os
-#import sys
-#print(sys.executable)
+areas = [  [5,    3,  85,105],
+           [107,  3, 187,105],
+           [209,  3, 289,105],
+           [5,  130,  85,235],
+           [107,130, 187,235],
+           [209,130, 289,235]]
 
-threshold_GLOBAL = 160
-
-def texto(img, texto, coord, fonte = cv2.FONT_HERSHEY_SIMPLEX, cor=(0,0,255), tamanho=1.7, thickness=2):
+def texto(img, texto, coord, fonte = cv2.FONT_HERSHEY_SIMPLEX, cor=(0,0,255), tamanho=0.5, thickness=2):
     textSize, baseline = cv2.getTextSize(texto, fonte, tamanho, thickness);
     cor_background = 0
     if type(cor)==int: # se não for colorida a imagem
         cor_background=255-cor
     else:
-        cor_background=(255-cor[0],255-cor[1],255-cor[2])
+        cor_background=(0,255,255)
     #print(cor_background)
     cv2.rectangle(img, (coord[0], coord[1]-textSize[1]-3), (coord[0]+textSize[0], coord[1]+textSize[1]-baseline), cor_background, -1)
     #cv2.putText(img, texto, coord, fonte, tamanho, cor_background, thickness+1, cv2.LINE_AA)
     cv2.putText(img, texto, coord, fonte, tamanho, cor, thickness, cv2.LINE_AA)
     return img
 
-def exibe(i,j=None,t1='Imagem',t2='Imagem Modificada'):
-    plt.figure(figsize=(16, 50)) # LARGGura e ALTura da imagem total em polegadas #fig=plt.figure(figsize=(18, 16), dpi= 80, facecolor='w', edgecolor='k')
-    plt.subplot2grid((1,2),(0,0)) #(linhas, colunas) do grid (l,c) do elemento
-    plt.title(t1)
-    plt.imshow(i)
-    plt.subplot2grid((1,2),(0,1)) #(linhas, colunas) do grid (l,c) do elemento
-    plt.title(t2)
-    plt.imshow(j)
+def subtraiArea(i1, i2, x1,y1,x2,y2):
+    iA = cv2.cvtColor(i1, cv2.COLOR_RGB2GRAY) # Converte para imagem em tons de cinza
+    iB = cv2.cvtColor(i2, cv2.COLOR_RGB2GRAY) 
+    result = iA[y1:y2,x1:x2] - iB[y1:y2,x1:x2] # realiza a subtração na área informada
+    soma_pixels_das_colunas = [sum(x) for x in zip(*result)] # soma os valores dos pixels das colunas
+    return True if sum(soma_pixels_das_colunas)>800000 else False
 
-def filtros(img):
-    blur = cv2.medianBlur(img, 15) #img_blobs = cv2.GaussianBlur(img_blobs, (7,7), 0) # aplica blur     #img_blobs = cv2.bilateralFilter(img, 3, 21, 21)
-    #exibe(img, blur, t1="Imagem original", t2="Blur")
-    #pb = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY) # converte para PB
-    pb = blur.copy()
-    #exibe(img, pb)
-    # Threshold ou Binarização
-    (T, bin) = cv2.threshold(pb, threshold_GLOBAL, 255, cv2.THRESH_BINARY)
-    #exibe(img, bin, t1="Imagem original", t2="Threshold")
-    ### Inverte preto e branco
-    img_inv = 255-bin # inverte a imagem
-    #exibe(img, img_inv, t1="Imagem original", t2="Imagem invertida")
-    return img_inv, blur
+def verificaVagas(img,img_clean):
+  for (a,b,c,d) in areas:
+    ocupada = subtraiArea(img,img_clean,a,b,c,d)
+    temp = 'Ocupada' if ocupada else 'Livre'
+    texto(img, temp, (a,b+15))
+  return img
 
-def imprime_hist(img): 
-    color = ('r','g','b')
-    fig = plt.figure(figsize=(6,3))
-    for i,col in enumerate(color):
-        histr = cv2.calcHist([img],[i],None,[256],[0,256])
-        plt.plot(histr,color = col)
-        plt.xlim([0,256])
-    
-    # If we haven't already shown or saved the plot, then we need to draw the figure first...
-    fig.canvas.draw()
-    # Now we can save it to a numpy array.
-    data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
-    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-    fig.clf()
-    plt.close()
-    return data
-
-def equaliza_imagem_colorida(img): #recebe em RGB
-    frame = cv2.cvtColor(img.copy(), cv2.COLOR_RGB2YCrCb)    # makes the blues image look real colored
-    i_YCrCb = frame.copy()
-    c0,c1,c2 = cv2.split(frame)
-    c0 = cv2.equalizeHist(c0)
-    frame = cv2.merge([c0,c1,c2])
-    frame = cv2.cvtColor(frame, cv2.COLOR_YCrCb2RGB)    # makes the blues image look real colored
-    return frame, i_YCrCb
-
-def imprime_canais(img):
-    (canalVermelho, canalVerde, canalAzul) = cv2.split(img)
-    zeros = np.zeros(img.shape[:2], dtype = "uint8")
-    #exibe(img,cv2.merge([zeros, zeros,canalVermelho]))
-    #exibe(cv2.merge([zeros, canalVerde, zeros]),cv2.merge([canalAzul, zeros, zeros]))
-    #exibe(img[:,:,0], img[:,:,2])
-    r = cv2.merge([canalVermelho, zeros, zeros])
-    g = cv2.merge([zeros, canalVerde, zeros])
-    b = cv2.merge([zeros, zeros, canalAzul])
-    return (r,g,b)
-    
+  
 #####################################
 ## INICIO MAIN
 #####################################
@@ -97,11 +50,16 @@ print('Bem vindo(a) ao Estacionamento Inteligente!')
 print('\n\n\nSelecione a fonte da imagem:')
 print('\n1. Câmera Pi')
 print('2. Webcam')
-#op = int(input('\nOpção: '))
-#input('\n\n\nLimpe a área de leitura para calibrar e selecione 1 para iniciar...')
-op=2 # para testes
+op = int(input('\nOpção: '))
+input('\n\n\nLimpe a área de leitura para calibrar e selecione 1 para iniciar...')
 
 if op==1:
+   ### PERGUNTA SE QUER VER AREAS DEMARCADAS
+   print('\n\nSelecione:')
+   print('\n1. Configurações')
+   print('2. Operação')
+   op = int(input('\nOpção: '))
+   
    try:
      from picamera.array import PiRGBArray
      from picamera import PiCamera
@@ -113,18 +71,22 @@ if op==1:
       
      # allow the camera to warmup
      time.sleep(0.1)
-
-       
       
      # capture frames from the camera
      for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
        # grab the raw NumPy array representing the image, then initialize the timestamp
        # and occupied/unoccupied text
        image = frame.array
-       image = image.copy()
-       #image = image.copy()
-       #image[10:60,:,:] = (255,0,0)
-       i = detectaCor_e_Forma(image)
+       i = image.copy()
+       
+       if op==1:
+         #Mostra configuracoes de areas
+         for (a,b,c,d) in areas:
+           i=cv2.rectangle(i.copy(),(a,b),(c,d),(0,255,0),2)
+       else:
+         #verifica vagas
+         i=verificaVagas(i, img_clean) #vai em formato RGB
+              
        # show the frame
        cv2.imshow("Frame", i)
        key = cv2.waitKey(1) & 0xFF
@@ -136,7 +98,7 @@ if op==1:
    except ImportError:
      print('Não esta rodando em um Raspberry')
 
-elif op==2:
+elif op==2: # USANDO A WEBCAM
   # Se não tem picamera então captura da webcam
   vc = cv2.VideoCapture(0)
   vc.set(cv2.CAP_PROP_FRAME_WIDTH,320)  
@@ -169,28 +131,34 @@ elif op==2:
       print('Erro!')
       vc.release()
 
+  ### PERGUNTA SE QUER VER AREAS DEMARCADAS
+  print('\n\nSelecione:')
+  print('\n1. Configurações')
+  print('2. Operação')
+  op = int(input('\nOpção: '))
 
   while is_capturing:
      try:    # Lookout for a keyboardInterrupt to stop the script
-         is_capturing, frame = vc.read()   
-
-         #sem usar calibragem
-         #frame = frame - img_clean
-
-         #frame = cv2.imread('circulo_amarelo.jpg')
-         #frame = cv2.imread('triangulo_vermelho.jpg')
-         #frame = cv2.imread('quadrado_azul.jpg')
-         #frame = cv2.resize(frame[:,:,::-1], (320,240))
-
-         i = detectaCor_e_Forma(frame[:,:,::-1]) #vai em formato RGB
-         window_name = "Formas"
-         #cv2.namedWindow(window_name, flags=cv2.WND_PROP_FULLSCREEN);
-
+         is_capturing, i = vc.read()   
+         #Rotaciona imagem
+         rows,cols,channels = i.shape
+         M = cv2.getRotationMatrix2D((cols/2,rows/2),180,1)
+         i = cv2.warpAffine(i,M,(cols,rows))
+         
+         if op==1:
+           #Mostra configuracoes de areas
+           for (a,b,c,d) in areas:
+             i=cv2.rectangle(i.copy(),(a,b),(c,d),(0,255,0),2)
+         else:
+           #verifica vagas
+           i=verificaVagas(i, img_clean) #vai em formato RGB
+                 
+         window_name = "Estacionamento"
          cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
          cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-
-         cv2.imshow(window_name, i[:,:,::-1]) #converte para BGR para mostrar
+         #cv2.imshow(window_name, i[:,:,::-1]) #converte para BGR para mostrar
+         cv2.imshow(window_name, i) #converte para BGR para mostrar
          key = cv2.waitKey(1) & 0xFF
          # if the `q` key was pressed, break from the loop
          if key == ord("q"):
